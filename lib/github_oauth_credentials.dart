1b59c7ffbbeeb0390bede73f5fb1770cee78d03d
lsandaruwanjayas <fgod406@gmail.com>
2025-03-11 18:00:03 +0530

    Add authentication

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,8 @@
+//Add client credentials to a new file, lib/github_oauth_credentials.dart
+
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liTNkds4vPngAkVh';
+const githubClientSecret = '7a6a5ee3214fcad02aa05977c67d650959043d9e';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
